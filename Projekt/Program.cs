2e17374fe5639d0ef5aac227fd024f4d8a7eac83
Filip Broniek <broniekf@gmail.com>
2021-07-21 16:04:40 +0200

    Skończyłem parsowanie klitkaUWitka

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Diagnostics.CodeAnalysis;
 
 namespace consoleasync
 {
@@ -100,32 +101,50 @@ namespace consoleasync
     }
 
     //nowy parser
+    class DishComparer : IEqualityComparer<Dish>
+    {
+        public bool Equals(Dish x, Dish y)
+        {
+            return string.Equals(x.Name, y.Name, StringComparison.CurrentCultureIgnoreCase);
+        }
 
+        public int GetHashCode([DisallowNull] Dish obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
     class DishParserGeneric
     {
-        public static IEnumerable<Dish> Parse(HtmlDocument doc, string basePath, Func<HtmlNode, string> findName)
+        public static IEnumerable<Dish> Parse(
+            HtmlDocument doc, 
+            string basePath, 
+            Func<HtmlNode, string> findName, 
+            Func<HtmlNode, Status> findAvailability)
         {
             var dishContainer = doc.DocumentNode.SelectSingleNode(basePath);
             var prices = FindPrices(dishContainer);
-            var dishes = new List<Dish>();
-            foreach (var price in prices)
-            {
-                var sPrice = price.InnerText.Trim().Trim(' ','z','ł');
-                //Console.WriteLine(sPrice);
-                var dPrice = Decimal.Parse(sPrice, new CultureInfo("pl-PL"));
-                var name = findName(price);
-                if (!string.IsNullOrWhiteSpace(name) && dPrice != 0)
-                {
-                    var item = new Dish { Name = name, Price = dPrice, Availability = Status.avalible };
+            return prices
+                .Select(p => CreateDish(p,findName,findAvailability))
+                .Where(d => d != null)
+                .Distinct(new DishComparer());
 
-                    dishes.Add(item);
-                }
-            }
-            //Console.WriteLine("");
-
-            return dishes;
         }
 
+        private static Dish CreateDish(HtmlNode price, Func<HtmlNode, string> findName, Func<HtmlNode, Status> findAvailability)
+        {
+            var sPrice = price.InnerText.Trim().Trim(' ', 'z', 'ł');
+            //Console.WriteLine(sPrice);
+            var dPrice = Decimal.Parse(sPrice, new CultureInfo("pl-PL"));
+            var name = findName(price);
+            var availability = findAvailability(price);
+            if (!string.IsNullOrWhiteSpace(name) && dPrice != 0)
+            {
+                var dish = new Dish { Name = name, Price = dPrice, Availability = availability };
+                return dish;
+
+            }
+            return null;
+        }
         private static IEnumerable<HtmlNode> FindPrices(HtmlNode dishContainer)
         {
             var children = dishContainer.ChildNodes;
@@ -148,35 +167,94 @@ namespace consoleasync
         public static async Task<IEnumerable<Dish>> FindDishes()
         {
             IEnumerable<Dish> dishes = null;
+            var baseXPath = "/html/body/div[1]/section/div/div[3]/div[2]/div[2]/div[1]/div[3]/div";
+            //Pizze
             for (var i=1; i<4; ++i)
             {
-                var client = new WebClient();
-                var downloadString1 = await client.DownloadStringTaskAsync($"https://klitkauwitka.pl/food/category/2016/pizza/page-{i}");
-
-                var doc = new HtmlDocument();
-                doc.LoadHtml(downloadString1);
-                var baseXPath = "/html/body/div[1]/section/div/div[3]/div[2]/div[2]/div[1]/div[3]/div";
-                var localDishes = DishParserGeneric.Parse(doc, baseXPath, FindName);
+                var client1 = new WebClient();
+                var downloadString1 = await client1.DownloadStringTaskAsync($"https://klitkauwitka.pl/food/category/2016/pizza/page-{i}");
+                var doc1 = new HtmlDocument();
+                doc1.LoadHtml(downloadString1);
+                var localDishes = DishParserGeneric.Parse(doc1, baseXPath, FindName, FindAvailability);
                 dishes = (i == 1 ? localDishes : dishes.Union(localDishes));
-                //dishes = dishes.Union(localDishes);
             }
+
+            //Sałatki
+            var client2 = new WebClient();
+            var downloadString2 = await client2.DownloadStringTaskAsync("https://klitkauwitka.pl/food/category/2246/salatki");
+            var doc2 = new HtmlDocument();
+            doc2.LoadHtml(downloadString2);
+            dishes = dishes.Union(DishParserGeneric.Parse(doc2, baseXPath, FindName, FindAvailability));
+
+            //Zapiekanki
+            var client3 = new WebClient();
+            var downloadString3 = await client3.DownloadStringTaskAsync("https://klitkauwitka.pl/food/category/1287/zapiekanki/page-1");
+            var doc3 = new HtmlDocument();
+            doc3.LoadHtml(downloadString3);
+            dishes = dishes.Union(DishParserGeneric.Parse(doc3, baseXPath, FindName, FindAvailability));
+
+            //Sosy do Pizzy
+            var client4 = new WebClient();
+            var downloadString4 = await client4.DownloadStringTaskAsync("https://klitkauwitka.pl/food/category/1288/sosy-do-pizzy");
+            var doc4 = new HtmlDocument();
+            doc4.LoadHtml(downloadString4);
+            dishes = dishes.Union(DishParserGeneric.Parse(doc4, baseXPath, FindName, FindAvailability));
+
+            //Zimne napoje
+            var client5 = new WebClient();
+            var downloadString5 = await client5.DownloadStringTaskAsync("https://klitkauwitka.pl/food/category/2119/zimne-napoje");
+            var doc5 = new HtmlDocument();
+            doc5.LoadHtml(downloadString5);
+            dishes = dishes.Union(DishParserGeneric.Parse(doc5, baseXPath, FindName, FindAvailability));
 
             return dishes;
         }
 
+        private static Status FindAvailability(HtmlNode price)
+        {
+            var availability = Status.avalible;
+            var sDishAvailability = price?.ParentNode?.ParentNode?.ParentNode?.ChildNodes?.FirstOrDefault()?.ChildNodes?.FirstOrDefault()?.InnerText;
+            switch (sDishAvailability)
+            {
+                case "Niedostępne":
+                    availability = Status.unavalible;
+                    break;
+                case "Chwilowo niedostępne":
+                    availability = Status.temporarilyUnavailable;
+                    break;
+            }
+            return availability;
+        }
 
         private static string FindName(HtmlNode price)
-        {
+        {           
             var dishName = price?.ParentNode?.ParentNode?.ParentNode?.ChildNodes?.FirstOrDefault()?.ChildNodes?.FirstOrDefault()?.InnerText;
+            switch (dishName)
+            {
+                case "Niedostępne":
+                case "Promocja!":
+                case "Chwilowo niedostępne":
+                    dishName = price?.ParentNode?.ParentNode?.ParentNode?.ChildNodes?.FirstOrDefault()?.ChildNodes?.ElementAtOrDefault(1)?
+                        .ChildNodes.FirstOrDefault().InnerText;
+                    if (string.IsNullOrEmpty(dishName))
+                    {
+                        dishName = price?.ParentNode?.ParentNode?.ChildNodes?.FirstOrDefault()?.ChildNodes?.FirstOrDefault()?
+                            .ChildNodes?.FirstOrDefault()?.InnerText;
+
+                        //console.writeline(dishname);
+                    }
+                    break;
+            }
+            var sPrice = price.InnerText;
+            dishName = dishName?.Replace(sPrice, string.Empty);
+            dishName = HttpUtility.HtmlDecode(dishName);
             if (!string.IsNullOrWhiteSpace(dishName))
             {
-                //Console.WriteLine(dishName);
                 return dishName == "20 cm: "? string.Empty : dishName;
             }
             return string.Empty;
         }
     }
-
     class Program
     {
         static async Task Main(string[] args)
